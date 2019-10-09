213dd31aa1b46ed61e4ebbc9f1900d8306beed06
EC2 Default User <ec2-user@ip-172-31-26-74.ap-southeast-1.compute.internal>
2019-10-09 15:39:23 +0000

    comment out 190331041

--- db/migrate/20190331041936_create_optiontables.rb
+++ db/migrate/20190331041936_create_optiontables.rb
@@ -1,11 +1,11 @@
 class CreateOptiontables < ActiveRecord::Migration[5.2]
   def change
-    create_table :optiontables do |t|
-      t.text        :name_opt
-      t.integer     :price_opt
-      t.text        :name_opt_zh
-      t.text        :name_opt_en
-      t.timestamps
-    end
+    # create_table :optiontables do |t|
+    #   t.text        :name_opt
+    #   t.integer     :price_opt
+    #   t.text        :name_opt_zh
+    #   t.text        :name_opt_en
+    #   t.timestamps
+    # end
   end
 end
