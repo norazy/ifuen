ef79a54444fb9e7ebc37758c51b506837a2a3860
EC2 Default User <ec2-user@ip-172-31-26-74.ap-southeast-1.compute.internal>
2019-10-10 04:58:37 +0000

    uncommented the commented parts

--- db/migrate/20190331084935_create_cashiers.rb
+++ db/migrate/20190331084935_create_cashiers.rb
@@ -1,9 +1,9 @@
 class CreateCashiers < ActiveRecord::Migration[5.2]
   def change
-    # create_table :cashiers do |t|
-    #   t.integer     :total, null: false
-    #   t.integer     :method, null: false
-    #   t.timestamps
-    # end
+    create_table :cashiers do |t|
+      t.integer     :total, null: false
+      t.integer     :method, null: false
+      t.timestamps
+    end
   end
 end
